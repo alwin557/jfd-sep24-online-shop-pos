bcc0c4f7e5f36e903070ee47f2d392654db97f11
alwin557 <jomiskids@yahoo.com>
2024-10-11 20:12:54 +0700

    mulai login

--- controller/c_auth.js
+++ controller/c_auth.js
@@ -1,7 +1,33 @@
+const model_user = require('../model/m_user')
+
 module.exports =
 {
 
-    halaman_login: function(reg,res) {
-        res.render('v_auth/login')
+    halaman_login: function(req,res) {
+        let data = {
+            notifikasi: req.query.notif,
+        }
+        res.render('v_auth/login', data)
+    },
+
+
+
+    proses_login: async function(req,res) {
+        // ambil inputan dari form login
+        let form_email      = req.body.form_email
+        let form_password   = req.body.form_password
+
+        // cek email yg diinput, ada gak di db
+        let email_exist = await model_user.cari_email(form_email)
+
+        if (email_exist.length > 0) {
+            // cek password
+            res.send('email ada')
+        } else {
+            // tendang ke halaman register
+            let pesan = `Email anda belum terdaftar, silakan registrasi lebih dulu!`
+            res.redirect(`/auth/login?notif=${pesan}`)
+        }
     }
+
 }
