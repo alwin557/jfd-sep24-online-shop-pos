2f80992c8b000df64aac9753e9e231664ae4dd0f
alwin557 <jomiskids@yahoo.com>
2024-10-14 19:08:37 +0700

    sampai bisa login

--- app.js
+++ app.js
@@ -1,9 +1,26 @@
 const express   = require('express')
 const app       = express()
 const port      = 3000
+const cookieparser = require('cookie-parser')
+const session   = require('express-session')
 const c_beranda = require('./controller/c_beranda')
 const c_auth    = require('./controller/c_auth')
+const cek_login = c_auth.cek_login
+const c_toko    = require('./controller/c_toko')
 
+// settingan untuk data session login
+app.use( cookieparser('rahasia'))
+app.use( session({
+    secret: 'rahasia',
+    resave: true,
+    saveUninitialized: false,
+    cookie: {
+        maxAge: (1000 * 60) * 30
+        // batas session expired:
+        // 1000 milidetik * 60 = 1 menit
+        // 1 menit * 30 = 1/2 jam
+    }
+}) )
 
 app.use( express.urlencoded({extended:false}) )
 app.use( express.static('public'))
@@ -14,6 +31,8 @@ app.set('views', './view')
 app.get('/', c_beranda.halaman_awal)
 app.get('/auth/login', c_auth.halaman_login)
 app.post('/auth/proses-login', c_auth.proses_login)
+
+app.get('/toko', cek_login, c_toko.index)
 
 app.listen(port, ()=>{
     console.log(`Aplikasi sudah siap, buka http://localhost:${port}`)
--- controller/c_auth.js
+++ controller/c_auth.js
@@ -1,33 +1,52 @@
-const model_user = require('../model/m_user')
+const bcrypt        = require('bcryptjs')
+const model_user    = require('../model/m_user')
 
 module.exports =
 {
 
     halaman_login: function(req,res) {
-        let data = {
-            notifikasi: req.query.notif,
+        if (req.session.user) {
+            res.redirect('/toko')
+        } else {
+            let data = {
+                notifikasi: req.query.notif,
+            }
+            res.render('v_auth/login', data)
         }
-        res.render('v_auth/login', data)
     },
-
-
 
     proses_login: async function(req,res) {
         // ambil inputan dari form login
         let form_email      = req.body.form_email
         let form_password   = req.body.form_password
-
         // cek email yg diinput, ada gak di db
         let email_exist = await model_user.cari_email(form_email)
 
         if (email_exist.length > 0) {
             // cek password
-            res.send('email ada')
+            let password_cocok = bcrypt.compareSync(form_password, email_exist[0].password)
+            if (password_cocok) {
+                // arahkan ke halaman utama sistem
+                req.session.user = email_exist
+                res.redirect('/toko')
+            } else {
+                // tendang ke halaman login
+                let pesan = `Password salah!`
+                res.redirect(`/auth/login?notif=${pesan}`)
+            }
         } else {
             // tendang ke halaman register
             let pesan = `Email anda belum terdaftar, silakan registrasi lebih dulu!`
             res.redirect(`/auth/login?notif=${pesan}`)
         }
-    }
-
+    },
+    cek_login: function(req,res,next) {
+        if (req.session.user) {
+            next()
+        } else {
+            // lempar ke halaman login
+            let pesan = 'sesi anda habis!'
+            res.redirect(`/auth/login?notif=${pesan}`)
+        }
+    },
 }
