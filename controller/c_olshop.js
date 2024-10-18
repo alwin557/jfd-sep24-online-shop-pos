b038437ae61ad36e1cf965fdd13df15159d180b0
alwin557 <jomiskids@yahoo.com>
2024-10-18 19:54:58 +0700

    isi kategori

--- controller/c_olshop.js
+++ controller/c_olshop.js
@@ -1,8 +1,13 @@
+const m_prod_kategori = require('../model/m_master_produk_kategori')
+
 module.exports =
 {
 
-    halaman_beranda: function(req,res)
-    {
-        res.render('v_olshop/beranda')
+    halaman_beranda: async function(req,res) {
+        let data = {
+            kategoriProduk: await m_prod_kategori.getSemua()
+        }
+        res.render('v_olshop/beranda', data)
     }
+
 }
